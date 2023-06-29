1322e5ef9ecb574210eebff5f88ea6ee4e460e24
Bronnikova98 <bronnikowa-wika@mail.ru>
2023-06-29 11:03:47 +0500

    feat: Pagination on the user exit page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::orderBy('created_at', 'desc')->get();
+        $users = User::orderBy('created_at', 'desc')->paginate(4);
         return view('users.index', compact('users'));
     }
 
